9f2c1301fb8a0d375e396de3c607652c5b002fa6
xrr-233 <ruixu33-c@my.cityu.edu.hk>
2023-02-14 15:36:36 +0800

    upd

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-     proxy: 'http://47.254.248.134:5000'
- }
+  transpileDependencies: true
 })
